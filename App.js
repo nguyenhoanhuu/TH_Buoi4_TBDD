04020d545aa7647d6adb1e5dc5cec418a7e5197d
Nguyen Hoan Huu <nguyenhoanhuu8668@gmail.com>
2022-09-22 11:16:54 +0700

    image result

--- App.js
+++ App.js
@@ -1,20 +1,161 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, ImageBackground, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image style={{ height: '100%' }} source={require('./img/book.png')}></Image>
+                <View style={styles.headerTextView}>
+                    <Text style={styles.headerText}>Nguyên hàm tích phân và ứng dụng</Text>
+                    <Text style={styles.headerText}>Cung cấp bởi Tiki Trading</Text>
+                    <Text style={[styles.headerText, { color: 'red' }]}>141.800 đ</Text>
+                   <View>
+                   <Text style={[styles.headerText,{textDecorationLine:'line-through',color: '#808080'}]}>141.800 đ</Text>
+                   </View>
+                    <View style={[styles.headerText, { flexDirection: 'row', justifyContent: 'space-between' }]}>
+                        <View style={{ flexDirection: 'row' }}>
+                            <TouchableOpacity
+                                style={{ width: 20, height: 20, alignItems: 'center', backgroundColor: '#C4C4C4' }}
+                            >
+                                <Text style={{ fontWeight: 'bold' }}>-</Text>
+                            </TouchableOpacity>
+                            <View
+                                style={{
+                                    width: 20,
+                                    height: 20,
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                }}
+                            >
+                                <Text>1</Text>
+                            </View>
+                            <TouchableOpacity
+                                style={{
+                                    width: 20,
+                                    height: 20,
+                                   
+                                    alignItems: 'center',
+                                    backgroundColor: '#C4C4C4',
+                                }}
+                            >
+                                <Text style={{ fontWeight: 'bold' }}>+</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <TouchableOpacity>
+                            <Text style={{ color: '#134FEC',fontWeight: 'bold'}}>Mua sau</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+            <View style={{ flexDirection: 'row', marginTop: 20 }}>
+                <Text style={{ fontWeight: 'bold', width: 140 }}>Mã giảm giá đã lưu</Text>
+                <TouchableOpacity style={{ marginLeft: 10 }}>
+                    <Text style={{ color: '#134FEC' }}>Xem tại đây</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 20 }}>
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        width: 280,
+                        height: 45,
+                        alignItems: 'center',
+                        marginLeft: 5,
+                        borderWidth: 1,
+                    }}
+                >
+                    <Text style={{ width: 30, height: 20, backgroundColor: 'yellow' }}></Text>
+                    <TextInput style={{ marginLeft: 10, width: 230, height: 45 }}>Mã giảm giá </TextInput>
+                </View>
+                <TouchableOpacity
+                    style={{
+                        marginLeft: 10,
+                        marginRight: 10,
+                        width: 100,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        backgroundColor: '#0A5EB7',
+                    }}
+                >
+                    <Text style={{ color: 'white', fontSize: 17, fontWeight: 'bold' }}>Áp dụng</Text>
+                </TouchableOpacity>
+            </View>
+            <Text style={{ width: '100%', height: 10, marginTop: 20, backgroundColor: '#C4C4C4' }}></Text>
+            <View style={styles.bodyContainer}>
+                <Text style={{ fontWeight: 'bold' }}>Bạn có phiếu quà tặng Tiki/Got it/ Urbox?</Text>
+                <TouchableOpacity style={{ marginLeft: 10 }}>
+                    <Text style={{ color: '#134FEC' }}>Nhập tại đây</Text>
+                </TouchableOpacity>
+            </View>
+            <Text style={{ width: '100%', height: 10, backgroundColor: '#C4C4C4' }}></Text>
+
+            <View
+                style={{ flexDirection: 'row', justifyContent: 'space-between', height: '10%', alignItems: 'center' }}
+            >
+                <Text style={{ fontSize: 18, fontWeight: 'bold', marginLeft: 20 }}>Tạm tính</Text>
+                <Text style={{ fontSize: 18, fontWeight: 'bold', color: 'red', marginRight: 20 }}>141.800 đ</Text>
+            </View>
+            <Text style={{ width: '100%', height: '15%', backgroundColor: '#C4C4C4' }}></Text>
+            <View
+                style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    height: '7%',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                }}
+            >
+                <Text style={{ color: '#808080', fontSize: 18, marginLeft: 20 }}>Thành tiền</Text>
+                <Text style={{ fontSize: 18, fontWeight: 'bold', color: 'red', marginRight: 20 }}>141.800 đ</Text>
+            </View>
+            <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: 35 }}>
+                <TouchableOpacity
+                    style={{
+                        width: '90%',
+                        height: 50,
+                        backgroundColor: 'red',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                >
+                    <Text style={{ color: 'white', fontWeight: 'bold' }}>TIẾN HÀNH ĐẶT HÀNG</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    header: {
+        marginTop: 30,
+        height: '20%',
+        width: '100%',
+        // borderWidth: 1,
+        flexDirection: 'row',
+    },
+    img: {
+        height: 20,
+    },
+    headerTextView: {
+        marginLeft: 20,
+    },
+    headerText: {
+        fontWeight: 'bold',
+        marginTop: 12,
+    },
+    bodyContainer: {
+        // marginTop: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '10%',
+        width: '100%',
+        // borderWidth: 1,
+        flexDirection: 'row',
+    },
 });
